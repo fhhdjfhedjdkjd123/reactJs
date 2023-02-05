ccff58ce7bdd485f0ac78641b6419b5f1dcb3b61
fhhdjfhedjdkjd123 <bash>
2023-02-05 14:18:45 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense Items!!!!!</h2>
+      <div>
+        <p>Food Rs10</p>
+        <p>Petrol Rs100</p>
+        <p>Movies Rs200</p>
+      </div>
+    </div>
+  );
+}
+export default ExpenseItem;
